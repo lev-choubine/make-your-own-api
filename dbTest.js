2c9876118a789f1cbbbd06369a5be237318f8ac3
Lev Michael <lev.choubine@gmail.com>
2020-12-17 13:45:54 -0500

    working on db connection issue

--- dbTest.js
+++ dbTest.js
@@ -1,8 +1,10 @@
-require ('dotenv').config()
+require('dotenv').config();
+const mongoose =require('mongoose');
+const express = require('express');
+const app = express();
 const models = require('./models')
 
-
-model.Bando.create({former_members: ['Hillel Slovak','Jack Irons','Cliff Martinez','Jack Sherman','DeWayne McKnight','D. H. Peligro','Arik Marshall','Jesse Tobias','Dave Navarro', 'Josh Klinghoffer'],
+models.Bando.create({former_members: ['Hillel Slovak','Jack Irons','Cliff Martinez','Jack Sherman','DeWayne McKnight','D. H. Peligro','Arik Marshall','Jesse Tobias','Dave Navarro', 'Josh Klinghoffer'],
 vocal: [{name: 'Anthony Kiedis', image: "https://ilarge.lisimg.com/image/2654625/740full-anthony-kiedis.jpg", joined_band: '1983'}],
 guitar:[{name: 'John Frusciante', image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/JohnFruscianteAugust2006.jpg/220px-JohnFruscianteAugust2006.jpg", joined_band: '1989'}] ,
 bass:[{name: 'Flea', image: "https://d26horl2n8pviu.cloudfront.net/link_data_pictures/images/000/334/371/shared_link/1200px-2016_RiP_Red_Hot_Chili_Peppers_-_Michael_Flea_Balzary_-_by_2eight_-_DSC0041.jpg?1539689224", joined_band: '1983'}] ,
